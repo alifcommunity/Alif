33e103465936c9860836a32cc9b4644427ceecd2
shad7ows <sha7dows@gmail.com>
2022-10-31 16:11:05 +0300

    تصحيح عمل مؤشر او محدد الخطأ
    تم تصحيح عمل محدد او مؤشر الخطأ

--- Errors.h
+++ Errors.h
@@ -6,30 +6,17 @@
 class ErrorArrow {
 public:
     std::wstring error_arrow(std::wstring input_, Position positionStart, Position positionEnd) {
-        std::wstring result;
-        int columnStart, columnEnd;
+        std::wstring line, result;
+        int indexStart, indexEnd, columnEnd;
 
-        int newlineIndex = input_.rfind(L"\n", positionStart.index);
-        int indexStart = std::max(newlineIndex, 0);
-        int indexEnd = input_.find(L"\n", indexStart + 1);
-        //if (indexEnd < 0) {
-        //    indexEnd = input_.length();
-        //}
+        indexStart = input_.rfind(L"\n", positionStart.index);
+        indexEnd = input_.find(L"\n", positionEnd.index);
+        columnEnd = positionEnd.columnNumber;
 
-        int lineCount = positionEnd.lineNumber - positionStart.lineNumber + 1;
-        for (int i = 0; i < lineCount; i++) {
-            std::wstring line = input_.substr(indexStart, indexEnd - indexStart); // تم طرح قيمة النهاية من البداية ليقوم بإختيار سطر واحد فقط ويجب إيجاد حل للمشكلة
-            if (i == 0) { columnStart = positionStart.columnNumber; }
-            else { columnStart = 0; }
-            if (i == lineCount - 1) { columnEnd = positionEnd.columnNumber + 1; }
-            else { columnEnd = line.length() - 1; }
+        line = input_.substr(indexStart + 1, indexEnd - indexStart);
 
-            result += line + L"\n" + std::wstring(columnStart, ' ') + std::wstring(columnEnd - columnStart, '^');
-
-            indexStart = indexEnd;
-            indexEnd = input_.find(L"\n", indexStart + 1);
-            if (indexEnd < 0) { indexEnd = input_.length(); }
-        }
+        result += line;
+        result += std::wstring(columnEnd, ' ') + std::wstring(1, '^');
 
         replace(result.begin(), result.end(), '\t', ' ');
         return result;
@@ -48,10 +35,8 @@ public:
 
     std::wstring print_() {
         std::wstring result = this->errorName + L": " + this->details + L"\n";
-        //result += L"الملف " + this->positionStart.fileName + L", السطر " + std::to_wstring(this->positionStart.lineNumber + 1);
         result += L"الملف " + fileName + L", السطر " + std::to_wstring(this->positionStart.lineNumber + 1);
-        //result += L"\n\n" + ErrorArrow().error_arrow(this->positionStart.input_, this->positionStart, this->positionEnd);
-        result += L"\n\n" + ErrorArrow().error_arrow(input, this->positionStart, this->positionEnd); // يجب ايجاد حل لاسم الملف المأخوذ من الموقع
+        result += L"\n\n" + ErrorArrow().error_arrow(input, this->positionStart, this->positionEnd);
 
         return result;
     }
