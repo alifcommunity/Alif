f0d803be8e0b6086814e7c717c242f49a66acf77
shad7ows <sha7dows@gmail.com>
2023-01-18 17:31:05 +0300

    إضافات وتحسينات
    تم العمل على قراءة الملف من ملف او الدخول الى حلقة تنفيذ البرنامج عن طريق الطرفية
    
    تم تصحيح خطأ يظهر في نظام النطاق عند استدعاء متغير عام
    
    حذف بعض التعليقات الغير ضرورية

--- source/Alif5.cpp
+++ source/Alif5.cpp
@@ -20,85 +20,22 @@
 #include "Errors.h"
 #include "Lexer.h"
 #include "Parser.h"
-
+#include "AlifRun.h"
 
 int main(int argc, char* argv[])
 {
-
-    bool outWText = _setmode(_fileno(stdout), _O_WTEXT);
-    bool inWText = _setmode(_fileno(stdin), _O_WTEXT);
-
-    if (!outWText and !inWText)
+    if (argc > 1)
     {
-        std::wcout << L"error" << std::endl;
-    } 
-
-    std::wstring input_;
-    std::wstring line;
-
-    std::wifstream fileContent("../source/AlifCode.alif5");
-    //std::wifstream fileContent(argv[1]);
-    fileContent.imbue(std::locale("ar_SA.UTF-8"));
-
-    // القراءة من ملف
-    /////////////////////////////////////////////////////////////////
-
-    while (std::getline(fileContent, line))
-    {
-        if (line != L"")
+        if (argc > 2)
         {
-            input_ += line;
-            input_ += L"\n";
+            prnt("يجب ان يتم تمرير اسم الملف فقط");
+            exit(-1);
         }
+
+        file_run(argv[1]);
     }
-    fileContent.close();
-
-    // القراءة من الكونسول
-    /////////////////////////////////////////////////////////////////
-
-    //while (true) {
-        //std::wcout << L"alif -> ";
-        //std::getline(std::wcin, input_);
-        
-    //std::wcout << input_ << std::endl;
-
-
-            // المعرب اللغوي
-            /////////////////////////////////////////////////////////////////
-
-            std::wstring fileName = L"AlifCode.Alif5";
-            Lexer lexer(fileName, input_);
-            lexer.make_token();
-            //lexer.print();
-
-
-            // المحلل اللغوي
-            /////////////////////////////////////////////////////////////////
-
-            Parser parser = Parser(&lexer.tokens_, fileName, input_);
-
-            clock_t start = clock(); // بداية حساب الوقت
-            
-            parser.parse();
-
-            std::wcout << float(clock() - start) / CLOCKS_PER_SEC << std::endl; // طباعة نتائج الوقت
-
-    //}
-
+    else
+    {
+        terminal_run();
+    }
 }
-
-
-// vectro [2.54, 2.54, 2.42] second in release
-// list [2.93, 3.05, 3.15] second in release
-
-// 1598401 line of this ->
-
-//#السلام عليكم
-//.
-//12345
-//اسم
-//"نص طويل الاجل"
-//ب += 4
-//ي -= 2
-//234 + 567 - 89 * 1 \ 2
-//(12 - 4, 43 * 5)
