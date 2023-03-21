cb6e4b5313dd8be389c7f642f07aa0cb2ba30037
shad7ows <sha7dows@gmail.com>
2023-03-21 17:02:05 +0300

    تصحيح مع إضافة بعض الدوال في المنفذ

--- source/AddrFuncs.h
+++ source/AddrFuncs.h
@@ -3,17 +3,31 @@
 typedef void(*Instructions_func)();
 
 void none_();
+
 void get_data();
 void set_data();
-void num_add();
-void num_minus();
-void str_add();
+
+void add_num();
+void minus_num();
+void mul_num();
+void div_num();
+void rem_num();
+void pow_num();
+
+void add_str();
 
 const Instructions_func instr_funcs[] = {
 	none_,
+
 	get_data,
 	set_data,
-	num_add,
-	num_minus,
-	str_add,
+	
+	add_num,
+	minus_num,
+	mul_num,
+	div_num,
+	rem_num,
+	pow_num,
+
+	add_str,
 };
