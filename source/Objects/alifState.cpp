45dda71b37bd5ddd88d46ae016aee630eabebb65
shad7ows <sha7dows@gmail.com>
2023-08-10 12:06:17 +0300

    جاري العمل على ملفات تشغيل ألف

--- source/Objects/alifState.cpp
+++ source/Objects/alifState.cpp
@@ -5,18 +5,50 @@
 
 
 #ifdef HAVE_DLOPEN
-#ifdef HAVE_DLFCN_H
-#include <dlfcn.h>
+	#ifdef HAVE_DLFCN_H
+	#include <dlfcn.h>
+	#endif
+	#if !HAVE_DECL_RTLD_LAZY
+	#define RTLD_LAZY 1
+	#endif
 #endif
-#if !HAVE_DECL_RTLD_LAZY
-#define RTLD_LAZY 1
-#endif
-#endif
 
 
-AlifStatus alifRuntimeState_init(AlifRuntimeState* runtime) {
+AlifStatus alifRuntimeState_init(AlifRuntimeState* _runtime)
+{
+	/* We preserve the hook across init, because there is
+	   currently no public API to set it between runtime
+	   initialization and interpreter initialization. */
+	void* openCodeHook = _runtime->openCodeHook;
+	void* openCodeUserData = _runtime->openCodeUserData;
+	AlifAuditHookEntry* auditHookHead = _runtime->auditHooks.head;
+	// Preserve nextIndex value if alif_initialize()/alif_finalize()
+	// is called multiple times.
+	AlifSizeT unicode_next_index = _runtime->unicodeState.ids.nextIndex;
 
+	AlifThreadTypeLock locks[NUMLOCKS];
+	if (alloc_forRuntime(locks) != 0) {
+		return ALIFSTATUS_NO_MEMORY();
+	}
 
+	if (_runtime->initialized) {
+		// alif_initialize() must be running again.
+		// Reset to AlifRuntimeState_INIT.
+		memcpy(_runtime, &initial, sizeof(*_runtime));
+	}
 
-	return (AlifStatus)0;
+	if (gilStateTss_init(_runtime) != 0) {
+		alifRuntimeState_fini(_runtime);
+		return ALIFSTATUS_NO_MEMORY();
+	}
+
+	if (alifThreadTss_create(&_runtime->trashTSSkey) != 0) {
+		alifRuntimeState_fini(_runtime);
+		return ALIFSTATUS_NO_MEMORY();
+	}
+
+	init_runtime(_runtime, openCodeHook, openCodeUserData, auditHookHead,
+		unicode_next_index, locks);
+
+	return ALIFSTATUS_OK();
 }
