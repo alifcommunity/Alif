a512afd78977828ebb4c0de15f4c311d61431b29
smo5ke <abdokteb.1@gmail.com>
2024-10-10 11:20:36 +0300

    انتهاء دالة alifFloat_fromDouble

---
+++ source/AlifCore/Include/Main/FloatObject.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+AlifTypeObject _alifFloatType_; // 14
+
+
+AlifObject* alifFloat_fromDouble(double); // 39
+
+
+
+
+
+
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+class AlifFloatObject { // 5
+public:
+	ALIFOBJECT_HEAD;
+	double fVal{};
+} ;
