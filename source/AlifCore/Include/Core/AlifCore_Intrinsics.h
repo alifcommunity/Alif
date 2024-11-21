9945380641e7945c4d29fd5150b5790462945fe4
shad7ows <sha7dows@gmail.com>
2024-11-21 11:40:06 +0300

    جاري العمل على مترجم اللغة

---
+++ source/AlifCore/Include/Core/AlifCore_Intrinsics.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+
+
+#define INTRINSIC_1_INVALID                      0
+#define INTRINSIC_PRINT                          1
+#define INTRINSIC_IMPORT_STAR                    2
+#define INTRINSIC_STOPITERATION_ERROR            3
+#define INTRINSIC_ASYNC_GEN_WRAP                 4
+#define INTRINSIC_UNARY_POSITIVE                 5
+#define INTRINSIC_LIST_TO_TUPLE                  6
+#define INTRINSIC_TYPEVAR                        7
+#define INTRINSIC_PARAMSPEC                      8
+#define INTRINSIC_TYPEVARTUPLE                   9
+#define INTRINSIC_SUBSCRIPT_GENERIC             10
--- source/AlifCore/Objects/AlifCompile.cpp
+++ source/AlifCore/Objects/AlifCompile.cpp
@@ -9,6 +9,7 @@
 #include "AlifCore_Compile.h"
 //#include "AlifCore_FlowGraph.h"
 #include "AlifCore_InstructionSequence.h"
+#include "AlifCore_Intrinsics.h"
 #include "AlifCore_State.h"
 
 #include "AlifCore_SymTable.h"
@@ -46,9 +47,21 @@ typedef AlifSourceLocation Location; // 99
 
 #define LOCATION(_lno, _endLno, _col, _endCol) {_lno, _endLno, _col, _endCol} // 108
 
+#define LOC(_x) SRC_LOCATION_FROM_AST(_x)
+
 typedef AlifJumpTargetLabel JumpTargetLabel; // 113
 
-static JumpTargetLabel _NoLabel_ = { -1 }; // 115
+static JumpTargetLabel _noLabel_ = { -1 }; // 115
+
+ // 120
+#define NEW_JUMP_TARGET_LABEL(_c, _name) \
+    JumpTargetLabel _name = _alifInstructionSequence_newLabel(INSTR_SEQUENCE(_c)); \
+    if (!IS_LABEL(_name)) { \
+        return ERROR; \
+    }
+
+#define USE_LABEL(_c, _lbl) \
+    RETURN_IF_ERROR(_alifInstructionSequence_useLabel(INSTR_SEQUENCE(_c), _lbl.id))
 
 
 enum FBlockType_ { // 137
@@ -177,9 +190,12 @@ public:
 
 
 static void compiler_free(AlifCompiler*); // 307
+static AlifCodeObject* compiler_mod(AlifCompiler*, ModuleTy); // 312
+static AlifIntT compiler_visitStmt(AlifCompiler*, StmtTy); // 313
+static AlifIntT compiler_visitExpr(AlifCompiler*, ExprTy); // 315
 
 
-#define CAPSULE_NAME "AlifCompile.cpp AlifCompiler unit"
+#define CAPSULE_NAME "AlifCompile.cpp AlifCompiler unit" // 360
 
 static AlifIntT compiler_setup(AlifCompiler* _c, ModuleTy _mod, AlifObject* _filename,
 	AlifCompilerFlags* _flags, AlifIntT _optimize, AlifASTMem* _astMem) { // 363
@@ -450,6 +466,13 @@ static AlifIntT codegen_addOpI(InstrSequence* seq,
 #define ADDOP_I(_c, _loc, _op, _o) \
     RETURN_IF_ERROR(codegen_addOpI(INSTR_SEQUENCE(_c), _op, _o, _loc)) // 715
 
+static AlifIntT codegen_addOpNoArg(InstrSequence* _seq,
+	AlifIntT _opcode, Location _loc) { // 721
+	return _alifInstructionSequence_addOp(_seq, _opcode, 0, _loc);
+}
+
+#define ADDOP(_c, _loc, _op) \
+    RETURN_IF_ERROR(codegen_addOpNoArg(INSTR_SEQUENCE(_c), _op, _loc)) // 729
 
 static AlifSizeT dict_addO(AlifObject* _dict, AlifObject* _o) { // 735
 	AlifObject* v{};
@@ -475,6 +498,27 @@ static AlifSizeT dict_addO(AlifObject* _dict, AlifObject* _o) { // 735
 	return arg;
 }
 
+
+
+
+#define ADDOP_LOAD_CONST(_c, _loc, _o) \
+    RETURN_IF_ERROR(codegen_addOpLoadConst(_c, _loc, _o)) // 906
+
+
+
+
+
+
+#define ADDOP_JUMP(_c, _loc, _op, _o) \
+    RETURN_IF_ERROR(codegen_addOpJ(INSTR_SEQUENCE(_c), _loc, _op, _o)) // 1010
+
+#define ADDOP_BINARY(_c, _loc, _binOp) \
+    RETURN_IF_ERROR(addop_binary(_c, _loc, _binOp, false)) // 1016
+
+
+ // 1035
+#define VISIT(_c, _type, _v) \
+    RETURN_IF_ERROR(compiler_visit ## _type(_c, _v));
 
 
 static AlifIntT compiler_enterScope(AlifCompiler* _c, Identifier _name,
@@ -647,6 +691,83 @@ static void compiler_exitScope(AlifCompiler* _c) { // 1197
 
 
 
+static AlifIntT compiler_body(AlifCompiler* _c, Location _loc, ASDLStmtSeq* _stmts) { // 1507
+	//if ((FUTURE_FEATURES(_c) & CO_FUTURE_ANNOTATIONS) and SYMTABLE_ENTRY(_c)->annotationsUsed) {
+	//	ADDOP(_c, _loc, SETUP_ANNOTATIONS);
+	//}
+	if (!ASDL_SEQ_LEN(_stmts)) {
+		return SUCCESS;
+	}
+	AlifSizeT firstInstr = 0;
+	//if (!IS_INTERACTIVE(_c)) {
+	//	AlifObject* docString = alifAST_getDocString(_stmts);
+	//	if (docString) {
+	//		firstInstr = 1;
+	//		/* if not -OO mode, set docstring */
+	//		if (OPTIMIZATION_LEVEL(_c) < 2) {
+	//			AlifObject* cleanDoc = alifCompile_cleanDoc(docString);
+	//			if (cleanDoc == nullptr) {
+	//				return ERROR;
+	//			}
+	//			StmtTy st = (StmtTy)ASDL_SEQ_GET(_stmts, 0);
+	//			Location loc = LOC(st->V.expression.val);
+	//			ADDOP_LOAD_CONST(_c, loc, cleanDoc);
+	//			ALIF_DECREF(cleanDoc);
+	//			RETURN_IF_ERROR(compiler_nameOp(_c, NO_LOCATION, &ALIF_ID(__doc__), ExprContext_::Store));
+	//		}
+	//	}
+	//}
+	for (AlifSizeT i = firstInstr; i < ASDL_SEQ_LEN(_stmts); i++) {
+		VISIT(_c, Stmt, (StmtTy)ASDL_SEQ_GET(_stmts, i));
+	}
+
+	//if (!(FUTURE_FEATURES(_c) & CO_FUTURE_ANNOTATIONS) and
+	//	_c->u_->deferredAnnotations != nullptr) {
+
+	//	AlifSTEntryObject* ste = SYMTABLE_ENTRY(_c);
+	//	AlifObject* deferredAnno = ALIF_NEWREF(_c->u_->deferredAnnotations);
+	//	void* key = (void*)((uintptr_t)ste->id + 1);
+	//	if (codegen_setupAnnotationsScope(_c, _loc, key,
+	//		ste->annotationBlock->name) == -1) {
+	//		ALIF_DECREF(deferredAnno);
+	//		return ERROR;
+	//	}
+	//	AlifSizeT annotations_len = alifList_size(deferredAnno);
+	//	for (AlifSizeT i = 0; i < annotations_len; i++) {
+	//		AlifObject* ptr = ALIFLIST_GET_ITEM(deferredAnno, i);
+	//		StmtTy st = (StmtTy)alifLong_asVoidPtr(ptr);
+	//		if (st == NULL) {
+	//			compiler_exitScope(_c);
+	//			ALIF_DECREF(deferredAnno);
+	//			return ERROR;
+	//		}
+	//		AlifObject* mangled = compiler_mangle(_c, st->V.annAssign.Target->V.name.id);
+	//		ADDOP_LOAD_CONST_NEW(_c, LOC(st), mangled);
+	//		VISIT(_c, expr, st->V.annAssign.annotation);
+	//	}
+	//	ALIF_DECREF(deferredAnno);
+
+	//	RETURN_IF_ERROR(
+	//		codegen_leaveAnnotationsScope(_c, _loc, annotations_len)
+	//	);
+	//	RETURN_IF_ERROR(
+	//		compiler_nameOp(_c, _loc, &ALIF_ID(__annotate__), ExprContext_::Store)
+	//	);
+	//}
+	return SUCCESS;
+}
+
+
+
+static Location start_location(ASDLStmtSeq* stmts) { // 1587
+	if (ASDL_SEQ_LEN(stmts) > 0) {
+		StmtTy st = (StmtTy)ASDL_SEQ_GET(stmts, 0);
+		return LOC(st);
+	}
+	return LOCATION(1, 1, 0, 0);
+}
+
+
 
 static AlifIntT compiler_codegen(AlifCompiler* _c, ModuleTy _mod) { // 1602
 	switch (_mod->type) {
@@ -662,7 +783,7 @@ static AlifIntT compiler_codegen(AlifCompiler* _c, ModuleTy _mod) { // 1602
 		break;
 	}
 	case ModK_::ExpressionK: {
-		VISIT(_c, expr, _mod->V.expression.body);
+		VISIT(_c, Expr, _mod->V.expression.body);
 		break;
 	}
 	default: {
@@ -703,6 +824,313 @@ finally:
 
 
 
+
+
+
+
+
+
+
+static AlifIntT codegen_stmtExpr(AlifCompiler* _c, Location _loc, ExprTy _value) { // 3797
+	//if (IS_INTERACTIVE(_c) and !IS_NESTED_SCOPE(_c)) {
+	//	VISIT(_c, Expr, _value);
+	//	ADDOP_I(_c, _loc, CALL_INTRINSIC_1, INTRINSIC_PRINT);
+	//	ADDOP(_c, NO_LOCATION, POP_TOP);
+	//	return SUCCESS;
+	//}
+
+	if (_value->type == ExprK_::ConstantK) {
+		/* ignore constant statement */
+		ADDOP(_c, _loc, NOP);
+		return SUCCESS;
+	}
+
+	VISIT(_c, Expr, _value);
+	ADDOP(_c, _noLocation_, POP_TOP); /* artificial */
+	return SUCCESS;
+}
+
+
+static AlifIntT compiler_visitStmt(AlifCompiler* _c, StmtTy _s) { // 3818
+	switch (_s->type) {
+	//case StmtK_::FunctionDefK:
+	//	return codegen_function(_c, _s, 0);
+	//case StmtK_::ClassDefK:
+	//	return codegen_class(_c, _s);
+	//case StmtK_::TypeAliasK:
+	//	return codegen_typealias(_c, _s);
+	//case StmtK_::ReturnK:
+	//	return codegen_return(_c, _s);
+	//case StmtK_::DeleteK:
+	//	VISIT_SEQ(_c, expr, _s->v.Delete.targets)
+	//		break;
+	//case StmtK_::AssignK:
+	//{
+	//	Py_ssize_t n = asdl_seq_LEN(_s->v.Assign.targets);
+	//	VISIT(_c, expr, _s->v.Assign.value);
+	//	for (Py_ssize_t i = 0; i < n; i++) {
+	//		if (i < n - 1) {
+	//			ADDOP_I(_c, LOC(_s), COPY, 1);
+	//		}
+	//		VISIT(_c, expr,
+	//			(expr_ty)asdl_seq_GET(_s->v.Assign.targets, i));
+	//	}
+	//	break;
+	//}
+	//case StmtK_::AugAssignK:
+	//	return codegen_augassign(_c, _s);
+	//case StmtK_::AnnAssignK:
+	//	return compiler_annassign(_c, _s);
+	//case StmtK_::ForK:
+	//	return codegen_for(_c, _s);
+	//case StmtK_::WhileK:
+	//	return codegen_while(_c, _s);
+	//case StmtK_::IfK:
+	//	return codegen_if(_c, _s);
+	//case StmtK_::MatchK:
+	//	return codegen_match(_c, _s);
+	//case StmtK_::RaiseK:
+	//{
+	//	Py_ssize_t n = 0;
+	//	if (_s->v.Raise.exc) {
+	//		VISIT(_c, expr, _s->v.Raise.exc);
+	//		n++;
+	//		if (_s->v.Raise.cause) {
+	//			VISIT(_c, expr, _s->v.Raise.cause);
+	//			n++;
+	//		}
+	//	}
+	//	ADDOP_I(_c, LOC(_s), RAISE_VARARGS, (int)n);
+	//	break;
+	//}
+	//case StmtK_::TryK:
+	//	return codegen_try(_c, _s);
+	//case StmtK_::TryStarK:
+	//	return codegen_try_star(_c, _s);
+	//case StmtK_::AssertK:
+	//	return codegen_assert(_c, _s);
+	//case StmtK_::ImportK:
+	//	return codegen_import(_c, _s);
+	//case StmtK_::ImportFromK:
+	//	return codegen_from_import(_c, _s);
+	//case StmtK_::GlobalK:
+	//case StmtK_::NonlocalK:
+	//	break;
+	case StmtK_::ExprK:
+	{
+		return codegen_stmtExpr(_c, LOC(_s), _s->V.expression.val);
+	}
+	//case StmtK_::PassK:
+	//{
+	//	ADDOP(_c, LOC(_s), NOP);
+	//	break;
+	//}
+	//case StmtK_::BreakK:
+	//{
+	//	return codegen_break(_c, LOC(_s));
+	//}
+	//case StmtK_::ContinueK:
+	//{
+	//	return codegen_continue(_c, LOC(_s));
+	//}
+	//case StmtK_::WithK:
+	//	return codegen_with(_c, _s, 0);
+	//case StmtK_::AsyncFunctionDefK:
+	//	return codegen_function(_c, _s, 1);
+	//case StmtK_::AsyncWithK:
+	//	return codegen_async_with(_c, _s, 0);
+	//case StmtK_::AsyncForK:
+	//	return codegen_async_for(_c, _s);
+	}
+
+	return SUCCESS;
+}
+
+
+
+
+
+
+
+static AlifIntT codegen_boolOp(AlifCompiler* _c, ExprTy _e) { // 4151
+	AlifIntT jumpi{};
+	AlifSizeT i{}, n{};
+	ASDLExprSeq* s{};
+
+	Location loc = LOC(_e);
+	if (_e->V.boolOp.op == And)
+		jumpi = POP_JUMP_IF_FALSE;
+	else
+		jumpi = POP_JUMP_IF_TRUE;
+	NEW_JUMP_TARGET_LABEL(_c, end);
+	s = _e->V.boolOp.vals;
+	n = ASDL_SEQ_LEN(s) - 1;
+	for (i = 0; i < n; ++i) {
+		VISIT(_c, Expr, (ExprTy)ASDL_SEQ_GET(s, i));
+		ADDOP_I(_c, loc, COPY, 1);
+		ADDOP(_c, loc, TO_BOOL);
+		ADDOP_JUMP(_c, loc, jumpi, end);
+		ADDOP(_c, loc, POP_TOP);
+	}
+	VISIT(_c, Expr, (ExprTy)ASDL_SEQ_GET(s, n));
+
+	USE_LABEL(_c, end);
+	return SUCCESS;
+}
+
+
+
+
+
+
+
+static AlifIntT compiler_visitExpr(AlifCompiler* _c, ExprTy _e) { // 5997
+	Location loc = LOC(_e);
+	switch (_e->type) {
+	case ExprK_::NamedExprK:
+		VISIT(_c, Expr, _e->V.namedExpr.val);
+		ADDOP_I(_c, loc, COPY, 1);
+		VISIT(_c, Expr, _e->V.namedExpr.target);
+		break;
+	case ExprK_::BoolOpK:
+		return codegen_boolOp(_c, _e);
+	case ExprK_::BinOpK:
+		VISIT(_c, Expr, _e->V.binOp.left);
+		VISIT(_c, Expr, _e->V.binOp.right);
+		ADDOP_BINARY(_c, loc, _e->V.binOp.op);
+		break;
+	case ExprK_::UnaryOpK:
+		VISIT(_c, Expr, _e->V.unaryOp.operand);
+		if (_e->V.unaryOp.op == UnaryOp_::UAdd) {
+			ADDOP_I(_c, loc, CALL_INTRINSIC_1, INTRINSIC_UNARY_POSITIVE);
+		}
+		else if (_e->V.unaryOp.op == UnaryOp_::Not) {
+			ADDOP(_c, loc, TO_BOOL);
+			ADDOP(_c, loc, UNARY_NOT);
+		}
+		else {
+			ADDOP(_c, loc, unaryop(_e->V.unaryOp.op));
+		}
+		break;
+	//case ExprK_::LambdaK:
+	//	return codegen_lambda(_c, _e);
+	case ExprK_::IfExprK:
+		return codegen_ifExpr(_c, _e);
+	case ExprK_::DictK:
+		return codegen_dict(_c, _e);
+	//case ExprK_::SetK:
+	//	return codegen_set(_c, _e);
+	//case ExprK_::GeneratorExpK:
+	//	return codegen_genexp(_c, _e);
+	//case ExprK_::ListCompK:
+	//	return codegen_listComp(_c, _e);
+	//case ExprK_::SetCompK:
+	//	return codegen_setComp(_c, _e);
+	//case ExprK_::DictCompK:
+	//	return codegen_dictComp(_c, _e);
+	//case ExprK_::YieldK:
+	//	if (!alifST_isFunctionLike(SYMTABLE_ENTRY(_c))) {
+	//		return compiler_error(_c, loc, "'yield' outside function");
+	//	}
+	//	if (_e->V.yield.val) {
+	//		VISIT(_c, Expr, _e->V.yield.val);
+	//	}
+	//	else {
+	//		ADDOP_LOAD_CONST(_c, loc, ALIF_NONE);
+	//	}
+	//	ADDOP_YIELD(_c, loc);
+	//	break;
+	//case ExprK_::YieldFromK:
+	//	if (!alifST_isFunctionLike(SYMTABLE_ENTRY(_c))) {
+	//		return compiler_error(_c, loc, "'yield from' outside function");
+	//	}
+	//	if (_c->u_->scopeType == COMPILER_SCOPE_ASYNC_FUNCTION) {
+	//		return compiler_error(_c, loc, "'yield from' inside async function");
+	//	}
+	//	VISIT(_c, Expr, _e->V.yieldFrom.val);
+	//	ADDOP(_c, loc, GET_YIELD_FROM_ITER);
+	//	ADDOP_LOAD_CONST(_c, loc, ALIF_NONE);
+	//	ADD_YIELD_FROM(_c, loc, 0);
+	//	break;
+	//case ExprK_::AwaitK:
+	//	VISIT(_c, Expr, _e->V.await.val);
+	//	ADDOP_I(_c, loc, GET_AWAITABLE, 0);
+	//	ADDOP_LOAD_CONST(_c, loc, ALIF_NONE);
+	//	ADD_YIELD_FROM(_c, loc, 1);
+	//	break;
+	//case ExprK_::CompareK:
+	//	return codegen_compare(_c, _e);
+	//case ExprK_::CallK:
+	//	return codegen_call(_c, _e);
+	case ExprK_::ConstantK:
+		ADDOP_LOAD_CONST(_c, loc, _e->V.constant.val);
+		break;
+	case ExprK_::JoinStrK:
+		return codegen_joinedStr(_c, _e);
+	case ExprK_::FormattedValK:
+		return codegen_formattedValue(_c, _e);
+		/* The following exprs can be assignment targets. */
+	//case ExprK_::AttributeK:
+	//	if (_e->V.attribute.ctx == ExprContext_::Load) {
+	//		AlifIntT ret = canOptimize_superCall(_c, _e);
+	//		RETURN_IF_ERROR(ret);
+	//		if (ret) {
+	//			RETURN_IF_ERROR(loadArgs_forSuper(_c, _e->V.attribute.val));
+	//			AlifIntT opcode = ASDL_SEQ_LEN(_e->V.attribute.val->V.call.args) ?
+	//				LOAD_SUPER_ATTR : LOAD_ZERO_SUPER_ATTR;
+	//			ADDOP_NAME(_c, loc, opcode, _e->V.attribute.attr, names);
+	//			loc = updateStart_locationToMatchAttr(_c, loc, _e);
+	//			ADDOP(_c, loc, NOP);
+	//			return SUCCESS;
+	//		}
+	//	}
+	//	RETURN_IF_ERROR(compiler_maybeAddStaticAttributeToClass(_c, _e));
+	//	VISIT(_c, Expr, _e->V.attribute.val);
+	//	loc = LOC(_e);
+	//	loc = updateStart_locationToMatchAttr(_c, loc, _e);
+	//	switch (_e->v.Attribute.ctx) {
+	//	case ExprContext_::Load:
+	//		ADDOP_NAME(_c, loc, LOAD_ATTR, _e->V.attribute.attr, names);
+	//		break;
+	//	case ExprContext_::Store:
+	//		ADDOP_NAME(_c, loc, STORE_ATTR, _e->V.attribute.attr, names);
+	//		break;
+	//	case ExprContext_::Del:
+	//		ADDOP_NAME(_c, loc, DELETE_ATTR, _e->V.attribute.attr, names);
+	//		break;
+	//	}
+	//	break;
+	//case ExprK_::SubScriptK:
+	//	return codegen_subscript(_c, _e);
+	//case ExprK_::StarK:
+	//	switch (_e->V.star.ctx) {
+	//	case Store:
+	//		/* In all legitimate cases, the Starred node was already replaced
+	//		 * by codegen_list/codegen_tuple. XXX: is that okay? */
+	//		return compiler_error(_c, loc,
+	//			"starred assignment target must be in a list or tuple");
+	//	default:
+	//		return compiler_error(_c, loc,
+	//			"can't use starred expression here");
+	//	}
+	//	break;
+	//case ExprK_::SliceK:
+	//{
+	//	AlifIntT n = codegen_slice(_c, _e);
+	//	RETURN_IF_ERROR(n);
+	//	ADDOP_I(_c, loc, BUILD_SLICE, n);
+	//	break;
+	//}
+	case ExprK_::NameK:
+		return compiler_nameOp(_c, loc, _e->V.name.name, _e->V.name.ctx);
+		/* child nodes of List and Tuple will have expr_context set */
+	case ExprK_::ListK:
+		return codegen_list(_c, _e);
+	case ExprK_::TupleK:
+		return codegen_tuple(_c, _e);
+	}
+	return SUCCESS;
+}
 
 
 
