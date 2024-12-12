c3c2b10d02fa7f67afb74b5fb757459555374388
Shadow <77246874+Shad7ows@users.noreply.github.com>
2024-12-11 21:54:54 -0800

    جاري العمل على مفسر اللغة

--- source/AlifCore/Include/Core/AlifCore_Frame.h
+++ source/AlifCore/Include/Core/AlifCore_Frame.h
@@ -29,7 +29,12 @@ public:
 
 
 
-
+enum FrameOwner { // 55
+	FRAME_OWNED_BY_THREAD = 0,
+	FRAME_OWNED_BY_GENERATOR = 1,
+	FRAME_OWNED_BY_FRAME_OBJECT = 2,
+	FRAME_OWNED_BY_CSTACK = 3,
+};
 
 
 
@@ -56,6 +61,42 @@ public:
 
 
 
+static inline AlifCodeObject* _alifFrame_getCode(AlifInterpreterFrame* _f) { // 81
+	return (AlifCodeObject*)_f->executable;
+}
+
 
 
 #define FRAME_SPECIALS_SIZE ((AlifIntT)((sizeof(AlifInterpreterFrame)-1)/sizeof(AlifObject*))) // 107
+
+
+
+
+
+
+
+
+
+
+
+
+
+static inline bool _alifFrame_isIncomplete(AlifInterpreterFrame* _frame) { // 212
+	if (_frame->owner == FrameOwner::FRAME_OWNED_BY_CSTACK) {
+		return true;
+	}
+	return _frame->owner != FrameOwner::FRAME_OWNED_BY_GENERATOR and
+		_frame->instrPtr < ALIFCODE_CODE(_alifFrame_getCode(_frame)) + _alifFrame_getCode(_frame)->firstTraceable;
+}
+
+static inline AlifInterpreterFrame* _alifFrame_getFirstComplete(AlifInterpreterFrame* _frame) { // 222
+	while (_frame and _alifFrame_isIncomplete(_frame)) {
+		_frame = _frame->previous;
+	}
+	return _frame;
+}
+
+
+static inline AlifInterpreterFrame* _alifThreadState_getFrame(AlifThread* _thread) { // 231
+	return _alifFrame_getFirstComplete(_thread->currentFrame);
+}
--- source/AlifCore/Include/Core/AlifCore_StackRef.h
+++ source/AlifCore/Include/Core/AlifCore_StackRef.h
@@ -25,3 +25,20 @@
 union AlifStackRef { // 52
 	uintptr_t bits{};
 };
+
+ // 57
+#define ALIF_TAG_DEFERRED (1)
+
+#define ALIF_TAG_PTR      (0)
+#define ALIF_TAG_BITS     (1)
+
+
+
+
+
+
+static inline AlifStackRef _alifStackRef_fromPyObjectSteal(AlifObject* _obj) { // 131
+	AlifIntT tag = (_obj == nullptr or ALIF_ISIMMORTAL(_obj)) ? (ALIF_TAG_DEFERRED) : ALIF_TAG_PTR;
+	return { .bits = ((uintptr_t)(_obj)) | tag };
+}
+#   define ALIFSTACKREF_FROMPYOBJECTSTEAL(_obj) _alifStackRef_fromPyObjectSteal(ALIFOBJECT_CAST(_obj))
