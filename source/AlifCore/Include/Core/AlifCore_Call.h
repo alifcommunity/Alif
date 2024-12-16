1d83380d73e1839c276804d77722ec097e37ee96
smo5ke <abdokteb.1@gmail.com>
2024-12-16 11:26:13 +0300

    العمل على alifObject_format

--- source/AlifCore/Include/Core/AlifCore_Call.h
+++ source/AlifCore/Include/Core/AlifCore_Call.h
@@ -25,20 +25,20 @@ static inline VectorCallFunc _alifVectorCall_functionInline(AlifObject* callable
 
 
 
-//static inline AlifObject* alifObject_vectorCallThread(AlifThread* tstate, AlifObject* callable,
-//	AlifObject* const* args, AlifUSizeT nargsf,
-//	AlifObject* kwnames) { 
-//	VectorCallFunc func;
-//	AlifObject* res;
-//
-//	func = alifVectorCall_functionInline(callable);
-//	if (func == nullptr) {
-//		AlifSizeT nargs = ALIFVECTORCALL_NARGS(nargsf);
-//		return alifObject_makeTpCall(tstate, callable, args, nargs, kwnames);
-//	}
-//	res = func(callable, args, nargsf, kwnames);
-//	return alif_checkFunctionResult(tstate, callable, res, nullptr);
-//}
+static inline AlifObject* alifObject_vectorCallThread(AlifThread* _tstate, AlifObject* _callable,
+	AlifObject* const* _args, AlifUSizeT _nArgsF,
+	AlifObject* _kWNames) { 
+	VectorCallFunc func{};
+	AlifObject* res_{};
+
+	func = _alifVectorCall_functionInline(_callable);
+	if (func == nullptr) {
+		AlifSizeT nargs = ALIFVECTORCALL_NARGS(_nArgsF);
+		return alifObject_makeTpCall(_tstate, _callable, _args, nargs, _kWNames);
+	}
+	res_ = func(_callable, _args, _nArgsF, _kWNames);
+	return alif_checkFunctionResult(_tstate, _callable, res_, nullptr);
+}
 //
 //
 //AlifObject* const* alifStack_unpackDict(AlifObject* const* args, int64_t nArgs, AlifObject* kwArgs, AlifObject** p_kwnames);
