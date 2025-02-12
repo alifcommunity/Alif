2d63c17124615b11c74c56145d5cce427cc2b806
smo5ke <abdokteb.1@gmail.com>
2025-02-12 13:04:09 +0300

    تصحيح

--- source/AlifCore/Objects/clinic/MathModule.cpp.h
+++ source/AlifCore/Objects/clinic/MathModule.cpp.h
@@ -2,16 +2,16 @@
 #include "AlifCore_ModSupport.h"
 
 
-// in file MathModule.c.h line 61
+// 61
 #define MATH_FACTORIAL_METHODDEF    \
     {"المضروب", (AlifCPPFunction)math_factorial, METHOD_O},
 
-
+// 479
 #define MATH_RADIANS_METHODDEF    \
     {"راديان", (AlifCPPFunction)math_radians, METHOD_O},
 
 
-static AlifObject* math_radiansImpl(AlifObject* , double );
+static AlifObject* math_radiansImpl(AlifObject* , double ); // 483
 
 static AlifObject* math_radians(AlifObject* _module, AlifObject* _arg) { // 486
 	AlifObject* returnValue = nullptr;
