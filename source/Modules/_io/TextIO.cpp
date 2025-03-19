b6edefffad95743d162382715e7b0eba815d7d45
shad7ows <sha7dows@gmail.com>
2025-03-19 23:49:44 +0300

    جاري العمل على نظام الإدخال والاخراج

---
+++ source/Modules/_io/TextIO.cpp
@@ -0,0 +1,103 @@
+#include "alif.h"
+
+#include "AlifCore_Call.h"
+#include "AlifCore_Codecs.h"
+#include "AlifCore_Interpreter.h"
+#include "AlifCore_Long.h"
+#include "AlifCore_Object.h"
+#include "AlifCore_Errors.h"
+#include "AlifCore_State.h"
+
+#include "_IOModule.h"
+
+
+
+
+
+
+
+
+
+
+typedef class TextIO TextIO;
+
+
+#include "clinic/textio.cpp.h" // 33
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* TextIOWrapper */
+
+typedef AlifObject* (*EncodeFuncT)(AlifObject*, AlifObject*); // 655
+
+class TextIO { // 658
+public:
+	ALIFOBJECT_HEAD{};
+	AlifIntT ok{}; /* initialized? */
+	AlifIntT detached{};
+	AlifSizeT chunkSize{};
+	AlifObject* buffer{};
+	AlifObject* encoding{};
+	AlifObject* encoder{};
+	AlifObject* decoder{};
+	AlifObject* readnl{};
+	AlifObject* errors{};
+	const char* writenl; /* ASCII-encoded; nullptr stands for \n */
+	char line_buffering;
+	char write_through;
+	char readuniversal;
+	char readtranslate;
+	char writetranslate;
+	char seekable;
+	char has_read1;
+	char telling;
+	char finalizing;
+	EncodeFuncT encodeFunc{};
+	char encodingStartOfStream{};
+	AlifObject* decoded_chars{};
+	AlifSizeT decodedCharsUsed{};
+	AlifObject* pendingBytes{};
+	AlifSizeT pendingBytesCount{};
+	AlifObject* snapshot{};
+	double b2cratIO{};
+
+	AlifObject* raw{};
+
+	AlifObject* weakRefList{};
+	AlifObject* dict{};
+
+	AlifIOState* state{};
+};
---
+++ source/Modules/_io/_IOModule.h
@@ -0,0 +1,59 @@
+#pragma once
+
+
+
+
+
+
+
+
+
+
+
+
+typedef class IOState AlifIOState; // 35
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class IOState { // 145
+public:
+	AlifIntT initialized{};
+	AlifObject* unsupportedOperation{};
+
+	/* Types */
+	AlifTypeObject* alifIOBaseType{};
+	AlifTypeObject* alifIncrementalNewlineDecoderType{};
+	AlifTypeObject* alifRawIOBaseType{};
+	AlifTypeObject* alifBufferedIOBaseType{};
+	AlifTypeObject* alifBufferedRWPairType{};
+	AlifTypeObject* alifBufferedRandomType{};
+	AlifTypeObject* alifBufferedReaderType{};
+	AlifTypeObject* alifBufferedWriterType{};
+	AlifTypeObject* alifBytesIOBufferType{};
+	AlifTypeObject* alifBytesIOType{};
+	AlifTypeObject* alifFileIOType{};
+	AlifTypeObject* alifStringIOType{};
+	AlifTypeObject* alifTextIOBaseType{};
+	AlifTypeObject* alifTextIOWrapperType{};
+#ifdef HAVE_WINDOWS_CONSOLE_IO
+	AlifTypeObject* alifWindowsConsoleIOType{};
+#endif
+};
