dd202fb90b9be3532a5732ccd5391843a15526e9
smo5ke <abdokteb.1@gmail.com>
2025-03-23 19:24:02 +0300

    العمل على تعديلات

--- source/Modules/_io/TextIO.cpp
+++ source/Modules/_io/TextIO.cpp
@@ -87,7 +87,7 @@ public:
 	char finalizing;
 	EncodeFuncT encodeFunc{};
 	char encodingStartOfStream{};
-	AlifObject* decoded_chars{};
+	AlifObject* decodedChars{};
 	AlifSizeT decodedCharsUsed{};
 	AlifObject* pendingBytes{};
 	AlifSizeT pendingBytesCount{};
@@ -103,19 +103,19 @@ public:
 };
 
 
-static AlifIntT _io_TextIOWrapper___init___impl(TextIO* self, AlifObject* buffer,
-	const char* encoding, AlifObject* errors,
-	const char* newline, AlifIntT line_buffering,
-	AlifIntT write_through) { // 1088
+static AlifIntT _ioTextIOWrapper_init_impl(TextIO* _self, AlifObject* _buffer,
+	const char* _encoding, AlifObject* _errors,
+	const char* _newLine, AlifIntT _lineBuffering,
+	AlifIntT _writeThrough) { // 1088
 	AlifObject* raw, * codeAlifInfo = nullptr;
 	AlifObject* res{};
 	AlifIntT r{};
 
-	self->ok = 0;
-	self->detached = 0;
+	_self->ok = 0;
+	_self->detached = 0;
 
-	if (encoding == nullptr) {
-		AlifInterpreter* interp = alifInterpreter_get();
+	if (_encoding == nullptr) {
+		AlifInterpreter* interp = _alifInterpreter_get();
 		if (alifInterpreter_getConfig(interp)->warnDefaultEncoding) {
 			//if (alifErr_warnEx(_alifExcEncodingWarning_,
 				//"'encoding' argument not specified", 1)) {
@@ -123,11 +123,10 @@ static AlifIntT _io_TextIOWrapper___init___impl(TextIO* self, AlifObject* buffer
 			//}
 		}
 	}
-
-	if (errors == ALIF_NONE) {
-		errors = &ALIF_ID(strict);
+	if (_errors == ALIF_NONE) {
+		_errors = &ALIF_ID(strict);
 	}
-	else if (!ALIFUSTR_CHECK(errors)) {
+	else if (!ALIFUSTR_CHECK(_errors)) {
 
 		//alifErr_format(
 			//_alifExcTypeError_,
@@ -135,48 +134,49 @@ static AlifIntT _io_TextIOWrapper___init___impl(TextIO* self, AlifObject* buffer
 			//ALIF_TYPE(errors)->name);
 		return -1;
 	}
-	else if (io_checkErrors(errors)) {
+	else if (io_checkErrors(_errors)) {
 		return -1;
 	}
-	const char* errorsStr = _alifUStr_asUTF8NoNUL(errors);
+	const char* errorsStr = _alifUStr_asUTF8NoNUL(_errors);
 	if (errorsStr == nullptr) {
 		return -1;
 	}
-
-	if (validate_newLine(newline) < 0) {
+	if (validate_newLine(_newLine) < 0) {
 		return -1;
 	}
 
-	ALIF_CLEAR(self->buffer);
-	ALIF_CLEAR(self->encoding);
-	ALIF_CLEAR(self->encoder);
-	ALIF_CLEAR(self->decoder);
-	ALIF_CLEAR(self->readnl);
-	ALIF_CLEAR(self->decoded_chars);
-	ALIF_CLEAR(self->pendingBytes);
-	ALIF_CLEAR(self->snapshot);
-	ALIF_CLEAR(self->errors);
-	ALIF_CLEAR(self->raw);
-	self->decodedCharsUsed = 0;
-	self->pendingBytesCount = 0;
-	self->encodeFunc = nullptr;
-	self->b2cratIO = 0.0;
+	ALIF_CLEAR(_self->buffer);
+	ALIF_CLEAR(_self->encoding);
+	ALIF_CLEAR(_self->encoder);
+	ALIF_CLEAR(_self->decoder);
+	ALIF_CLEAR(_self->readnl);
+	ALIF_CLEAR(_self->decodedChars);
+	ALIF_CLEAR(_self->pendingBytes);
+	ALIF_CLEAR(_self->snapshot);
+	ALIF_CLEAR(_self->errors);
+	ALIF_CLEAR(_self->raw);
+	_self->decodedCharsUsed = 0;
+	_self->pendingBytesCount = 0;
+	_self->encodeFunc = nullptr;
+	_self->b2cratIO = 0.0;
 
-	if (encoding == nullptr or (strcmp(encoding, "locale") == 0)) {
-		self->encoding = alif_getLocaleEncodingObject();
-		if (self->encoding == nullptr) {
+	if (_encoding == nullptr and _alifDureRun_.preConfig.utf8Mode) {
+		_self->encoding = &ALIF_STR(utf8);
+	}
+	if (_encoding == nullptr or (strcmp(_encoding, "locale") == 0)) {
+		_self->encoding = alif_getLocaleEncodingObject();
+		if (_self->encoding == nullptr) {
 			goto error;
 		}
 	}
-
-	if (self->encoding != nullptr) {
-		encoding = alifUStr_asUTF8(self->encoding);
-		if (encoding == nullptr)
+	if (_self->encoding != nullptr) {
+		_encoding = alifUStr_asUTF8(_self->encoding);
+		if (_encoding == nullptr)
 			goto error;
 	}
-	else if (encoding != nullptr) {
-		self->encoding = alifUStr_fromString(encoding);
-		if (self->encoding == nullptr)
+	else if (_encoding != nullptr) {
+		_self->encoding = alifUStr_fromString(_encoding);
+		if (_self->encoding == nullptr)
 			goto error;
 	}
 	else {
@@ -185,67 +185,67 @@ static AlifIntT _io_TextIOWrapper___init___impl(TextIO* self, AlifObject* buffer
 		goto error;
 	}
 
-	codeAlifInfo = _alifCodec_lookupTextEncoding(encoding, "codecs.open()");
+	codeAlifInfo = _alifCodec_lookupTextEncoding(_encoding, "codecs.open()");
 	if (codeAlifInfo == nullptr) {
-		ALIF_CLEAR(self->encoding);
+		ALIF_CLEAR(_self->encoding);
 		goto error;
 	}
 
-	self->errors = ALIF_NEWREF(errors);
-	self->chunkSize = 8192;
-	self->line_buffering = line_buffering;
-	self->write_through = write_through;
-	if (set_newLine(self, newline) < 0) {
+	_self->errors = ALIF_NEWREF(_errors);
+	_self->chunkSize = 8192;
+	_self->line_buffering = _lineBuffering;
+	_self->write_through = _writeThrough;
+	if (set_newLine(_self, _newLine) < 0) {
 		goto error;
 	}
 
-	self->buffer = ALIF_NEWREF(buffer);
+	_self->buffer = ALIF_NEWREF(_buffer);
 
-	_PyIO_State* state = findIO_stateByDef(ALIF_TYPE(self));
-	self->state = state;
-	if (textIOWrapper_setDecoder(self, codeAlifInfo, errorsStr) != 0)
+	_PyIO_State* state = findIO_stateByDef(ALIF_TYPE(_self));
+	_self->state = state;
+	if (textIOWrapper_setDecoder(_self, codeAlifInfo, errorsStr) != 0)
 		goto error;
 
-	if (textIOWrapper_setEncoder(self, codeAlifInfo, errorsStr) != 0)
+	if (textIOWrapper_setEncoder(_self, codeAlifInfo, errorsStr) != 0)
 		goto error;
 
 	ALIF_CLEAR(codeAlifInfo);
 
-	if (ALIF_IS_TYPE(buffer, state->alifBufferedReaderType) or
-		ALIF_IS_TYPE(buffer, state->alifBufferedWriterType) or
-		ALIF_IS_TYPE(buffer, state->alifBufferedRandomType))
+	if (ALIF_IS_TYPE(_buffer, state->alifBufferedReaderType) or
+		ALIF_IS_TYPE(_buffer, state->alifBufferedWriterType) or
+		ALIF_IS_TYPE(_buffer, state->alifBufferedRandomType))
 	{
-		if (alifObject_getOptionalAttr(buffer, &ALIF_ID(raw), &raw) < 0)
+		if (alifObject_getOptionalAttr(_buffer, &ALIF_ID(raw), &raw) < 0)
 			goto error;
 		if (raw != nullptr) {
 			if (ALIF_IS_TYPE(raw, state->PyFileIO_Type))
-				self->raw = raw;
+				_self->raw = raw;
 			else
 				ALIF_DECREF(raw);
 		}
 	}
 
-	res = alifObject_callMethodNoArgs(buffer, &ALIF_ID(seekable));
+	res = alifObject_callMethodNoArgs(_buffer, &ALIF_ID(seekable));
 	if (res == nullptr)
 		goto error;
 	r = alifObject_isTrue(res);
 	ALIF_DECREF(res);
 	if (r < 0)
 		goto error;
-	self->seekable = self->telling = r;
+	_self->seekable = _self->telling = r;
 
-	r = alifObject_hasAttrWithError(buffer, &ALIF_ID(read1));
+	r = alifObject_hasAttrWithError(_buffer, &ALIF_ID(read1));
 	if (r < 0) {
 		goto error;
 	}
-	self->has_read1 = r;
+	_self->has_read1 = r;
 
-	self->encodingStartOfStream = 0;
-	if (textIOWrapper_fixEncoderState(self) < 0) {
+	_self->encodingStartOfStream = 0;
+	if (textIOWrapper_fixEncoderState(_self) < 0) {
 		goto error;
 	}
 
-	self->ok = 1;
+	_self->ok = 1;
 	return 0;
 
 error:
