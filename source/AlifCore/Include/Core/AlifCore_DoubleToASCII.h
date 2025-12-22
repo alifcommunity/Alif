8e3bd1ef8123730bfdf21eac561a82bdb57e6a36
shad7ows <sha7dows@gmail.com>
2025-12-22 12:32:34 +0300

    تصحيح تحويل النوع

--- source/AlifCore/Include/Core/AlifCore_DoubleToASCII.h
+++ source/AlifCore/Include/Core/AlifCore_DoubleToASCII.h
@@ -14,7 +14,7 @@ struct Bigint { // 16
 };
 
 
- // 33
+// 33
 #define BIGINT_KMAX 7
 
 #define BIGINT_POW5SIZE 8
@@ -41,7 +41,7 @@ public:
 
 
 extern double _alif_dgStrToDouble(const char*, char**); // 62
-extern char* _alif_dgDoubletoASCII(double, int, int, int*, int*, char**); // 63
+extern char* _alif_dgDoubletoASCII(double, int, int, AlifIntT*, AlifIntT*, char**); // 63
 extern void _alif_dgFreeDoubleToASCII(char*); // 65
 
 
